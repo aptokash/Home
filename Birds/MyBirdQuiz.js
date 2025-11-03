83ef95a8751a8ce4132128f2cf0b9b07b7c96b85
Andrew Tokash <54639709+aptokash@users.noreply.github.com>
2025-11-03 12:27:33 -0500

    Add files via upload

---
+++ Birds/MyBirdQuiz.js
@@ -0,0 +1,35 @@
+sparrows = [
+
+	"www.lycobirds.com/id-quiz/2025-03-30",
+	"www.lycobirds.com/id-quiz/2025-04-09",
+	"www.lycobirds.com/id-quiz/2023-06-29",
+	"www.lycobirds.com/id-quiz/2024-03-06",
+	"www.lycobirds.com/id-quiz/2025-04-28",
+	"www.lycobirds.com/id-quiz/2022-02-15",
+	"www.lycobirds.com/id-quiz/2021-10-05",
+	"www.lycobirds.com/id-quiz/2025-10-14",
+	"www.lycobirds.com/id-quiz/2021-04-06",
+	"www.lycobirds.com/id-quiz/2025-10-24"
+
+];
+
+waterfowl = [
+	"www.lycobirds.com/id-quiz/2025-11-03",
+	"www.lycobirds.com/id-quiz/2025-01-23",
+	"www.lycobirds.com/id-quiz/2024-07-24",
+	"www.lycobirds.com/id-quiz/2025-10-31",
+	"www.lycobirds.com/id-quiz/2025-11-03"
+];	
+
+other = [
+	"www.lycobirds.com/id-quiz/2025-09-08", 	// thrasher
+	"www.lycobirds.com/id-quiz/2025-10-25" 		// wren
+
+];	
+
+	
+	
+
+
+
+
