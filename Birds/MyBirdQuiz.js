ef27ab35b5fe570af90467b0ffb3095ef5179e34
Andrew Tokash <54639709+aptokash@users.noreply.github.com>
2025-11-07 16:44:22 -0500

    Add files via upload

--- Birds/MyBirdQuiz.js
+++ Birds/MyBirdQuiz.js
@@ -2,30 +2,37 @@ sparrows = [
 
 	"www.lycobirds.com/id-quiz/2025-03-30",
 	"www.lycobirds.com/id-quiz/2025-04-09",
-	"www.lycobirds.com/id-quiz/2023-06-29",
-	"www.lycobirds.com/id-quiz/2024-03-06",
 	"www.lycobirds.com/id-quiz/2025-04-28",
+	"www.lycobirds.com/id-quiz/2025-10-24",
+	"www.lycobirds.com/id-quiz/2025-10-14",
+	"www.lycobirds.com/id-quiz/2025-11-05",
+
+	"www.lycobirds.com/id-quiz/2024-03-06",
+	"www.lycobirds.com/id-quiz/2023-06-29",
 	"www.lycobirds.com/id-quiz/2022-02-15",
 	"www.lycobirds.com/id-quiz/2021-10-05",
-	"www.lycobirds.com/id-quiz/2025-10-14",
 	"www.lycobirds.com/id-quiz/2021-04-06",
-	"www.lycobirds.com/id-quiz/2025-10-24"
 
-];
+	"www.lycobirds.com/id-quiz/2021-09-22",
 
-waterfowl = [
+
+"x"];waterfowl = [
+
 	"www.lycobirds.com/id-quiz/2025-11-03",
 	"www.lycobirds.com/id-quiz/2025-01-23",
-	"www.lycobirds.com/id-quiz/2024-07-24",
 	"www.lycobirds.com/id-quiz/2025-10-31",
-	"www.lycobirds.com/id-quiz/2025-11-03"
-];	
+	"www.lycobirds.com/id-quiz/2025-11-03",
+	"www.lycobirds.com/id-quiz/2025-11-06",
 
-other = [
+	"www.lycobirds.com/id-quiz/2024-07-24",
+
+
+"x"];other = [
+	
 	"www.lycobirds.com/id-quiz/2025-09-08", 	// thrasher
-	"www.lycobirds.com/id-quiz/2025-10-25" 		// wren
+	"www.lycobirds.com/id-quiz/2025-10-25", 	// wren
 
-];	
+"X"];	
 
 	
 	
